58b08d3b5ab494ab77a7f764831c16e8b8b26ed9
MagdaWu <wumagda16@gmail.com>
2019-05-27 23:54:20 +0200

    Zarodkowanie

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -41,16 +41,20 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -64,10 +68,11 @@
             this.pictureBox1.Size = new System.Drawing.Size(500, 500);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(643, 15);
+            this.button1.Location = new System.Drawing.Point(795, 461);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 54);
             this.button1.TabIndex = 1;
@@ -77,9 +82,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(755, 122);
+            this.textBox1.Location = new System.Drawing.Point(97, 69);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(63, 22);
+            this.textBox1.Size = new System.Drawing.Size(66, 22);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
@@ -92,7 +97,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(689, 122);
+            this.label1.Location = new System.Drawing.Point(14, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 17);
             this.label1.TabIndex = 4;
@@ -110,10 +115,11 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(692, 84);
+            this.comboBox1.Location = new System.Drawing.Point(661, 84);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(164, 24);
             this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
             // 
             // label3
             // 
@@ -126,22 +132,22 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(575, 153);
+            this.textBox3.Location = new System.Drawing.Point(901, 84);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(63, 22);
+            this.textBox3.Size = new System.Drawing.Size(66, 22);
             this.textBox3.TabIndex = 8;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(575, 119);
+            this.textBox4.Location = new System.Drawing.Point(1046, 84);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(63, 22);
+            this.textBox4.Size = new System.Drawing.Size(66, 22);
             this.textBox4.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(532, 122);
+            this.label4.Location = new System.Drawing.Point(858, 87);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 17);
             this.label4.TabIndex = 10;
@@ -150,7 +156,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(532, 156);
+            this.label5.Location = new System.Drawing.Point(1005, 87);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 17);
             this.label5.TabIndex = 11;
@@ -162,35 +168,12 @@
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.textBox6);
             this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Location = new System.Drawing.Point(535, 197);
+            this.groupBox1.Location = new System.Drawing.Point(535, 126);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(189, 104);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Jednorodne";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(99, 27);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(66, 22);
-            this.textBox5.TabIndex = 0;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(99, 65);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(66, 22);
-            this.textBox6.TabIndex = 1;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 30);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 17);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Wiersz";
             // 
             // label7
             // 
@@ -201,25 +184,41 @@
             this.label7.TabIndex = 3;
             this.label7.Text = "Kolumna";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Wiersz";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(99, 65);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(66, 22);
+            this.textBox6.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(99, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(66, 22);
+            this.textBox5.TabIndex = 0;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.textBox7);
             this.groupBox2.Controls.Add(this.textBox2);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(535, 329);
+            this.groupBox2.Location = new System.Drawing.Point(730, 126);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(189, 111);
+            this.groupBox2.Size = new System.Drawing.Size(189, 104);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Z promieniem";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(99, 69);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(66, 22);
-            this.textBox7.TabIndex = 6;
             // 
             // label8
             // 
@@ -230,38 +229,85 @@
             this.label8.TabIndex = 7;
             this.label8.Text = "Ilość";
             // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(99, 69);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(66, 22);
+            this.textBox7.TabIndex = 6;
+            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label9);
             this.groupBox3.Controls.Add(this.textBox8);
-            this.groupBox3.Location = new System.Drawing.Point(778, 204);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(925, 126);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(163, 64);
+            this.groupBox3.Size = new System.Drawing.Size(187, 104);
             this.groupBox3.TabIndex = 14;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Losowe";
             // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(70, 29);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(76, 22);
-            this.textBox8.TabIndex = 0;
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 32);
+            this.label9.Location = new System.Drawing.Point(14, 30);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(36, 17);
             this.label9.TabIndex = 1;
             this.label9.Text = "Ilość";
             // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(97, 27);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(66, 22);
+            this.textBox8.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(528, 255);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 17);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Warunki brzegowe";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(659, 252);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(166, 24);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(528, 304);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 17);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Sąsiedztwo";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(659, 301);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(166, 24);
+            this.comboBox3.TabIndex = 18;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1042, 532);
+            this.ClientSize = new System.Drawing.Size(1174, 611);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -271,8 +317,6 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -314,6 +358,10 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -39,16 +39,24 @@ namespace WindowsFormsApplication3
             DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             pictureBox1.Image = DrawArea;
             g = Graphics.FromImage(DrawArea);
+
             var rand = new Random();
             for (int i = 0; i < 101; i++)
             {
                 Color randomColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
                 brushes.Add(new SolidBrush(randomColor));
             }
-            beginTab = new List<List<int>>();
-            
-            nextTab = new List<List<int>>();
-            
+
+            comboBox2.Items.Add("Periodyczne");
+            comboBox2.Items.Add("Absorbujące");
+
+            comboBox3.Items.Add("Moore");
+            comboBox3.Items.Add("Von Neumann");
+            comboBox3.Items.Add("Heks L");
+            comboBox3.Items.Add("Heks P");
+            comboBox3.Items.Add("Heks Los");
+            comboBox3.Items.Add("Pent Los");
+
 
             comboBox1.Items.Add("Jednorodne");
             comboBox1.Items.Add("Z promieniem");
@@ -115,20 +123,8 @@ namespace WindowsFormsApplication3
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            int a = int.Parse(textBox1.Text);
-            int b = int.Parse(textBox2.Text);
-            wys = int.Parse(textBox3.Text);
-            szer = int.Parse(textBox4.Text);
-            for (int i = 0; i < szer; i++)
-            {
-                beginTab.Add(Enumerable.Repeat<int>(0, wys).ToList());
-            }
-            for (int i = 0; i < szer; i++)
-            {
-                nextTab.Add(Enumerable.Repeat<int>(0, wys).ToList());
-            }
-            GenerateRandomSeeds(a, b);
+        {         
+            
             if (timer.Enabled)
             {
                 timer.Stop();
@@ -137,8 +133,53 @@ namespace WindowsFormsApplication3
                 timer.Start();
         }
 
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            g.Clear(Color.Transparent);
+
+            beginTab = new List<List<int>>();
+            nextTab = new List<List<int>>();
+
+            wys = int.Parse(textBox3.Text);
+            szer = int.Parse(textBox4.Text);
+
+            for (int i = 0; i < szer; i++)
+            {
+                beginTab.Add(Enumerable.Repeat<int>(0, wys).ToList());
+            }
+            for (int i = 0; i < szer; i++)
+            {
+                nextTab.Add(Enumerable.Repeat<int>(0, wys).ToList());
+            }
+
+            if (comboBox1.Text == "Jednorodne")
+            {
+                int wiersz = int.Parse(textBox5.Text);
+                int kolumna = int.Parse(textBox6.Text);
+                GenerateHomogeneousSeeds(wiersz, kolumna);
+            }
+            else if (comboBox1.Text == "Z promieniem")
+            {
+                int promien = int.Parse(textBox2.Text);
+                int promIle = int.Parse(textBox7.Text);
+                GenerateRadiusSeeds(promien, promIle);
+            }
+            else if (comboBox1.Text == "Losowe")
+            {
+                int nrTypes = int.Parse(textBox1.Text);
+                int losIle = int.Parse(textBox8.Text);
+                GenerateRandomSeeds(nrTypes, losIle);
+            }
+            else if (comboBox1.Text == "Klikanie")
+            {
+
+            }
+        }
+
+
         public void GenerateRandomSeeds(int numberOfTypes, int numberOfSeeds)
         {
+            nextState = 1;
             var rand = new Random();
             for (int i = 1; i <= numberOfTypes; i++)
             {
@@ -160,6 +201,91 @@ namespace WindowsFormsApplication3
             }
             pictureBox1.Image = DrawArea;
         }
+        public void GenerateHomogeneousSeeds(int numberRows, int numberCols)
+        {
+            nextState = 1;
+            int rowOffset = szer / numberRows;
+            int colOffset = wys / numberCols;
 
+            for (int i = 0; i < numberRows; i++)
+            {
+                for (int j = 0; j < numberCols; j++)
+                {
+                    Draw((int)((i + 0.5) * rowOffset), (int)((j + 0.5) * colOffset), nextState);
+                    nextTab[(int)((i + 0.5) * rowOffset)][(int)((j + 0.5) * colOffset)] = nextState++;
+                }
+            }
+        }
+        public void GenerateRadiusSeeds(int radius, int number)
+        {
+            nextState = 1;
+            var rand = new Random();
+          
+            List<Tuple<int, int>> points = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    int row = rand.Next(szer);
+                    int col = rand.Next(wys);
+                    bool isValid = true;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        double distance = Math.Sqrt(Math.Pow(row- points[k].Item1,2)+Math.Pow(col-points[k].Item2,2));
+                        if (distance <= radius * 2)
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    if (isValid)
+                    {
+                        points.Add(new Tuple<int, int>(row, col));
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Draw(points[i].Item1, points[i].Item2, nextState);
+                nextTab[points[i].Item1][points[i].Item2] = nextState++;
+            }
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs myszka = (MouseEventArgs)e;
+            Point coordinates = myszka.Location;
+
+            wys = int.Parse(textBox3.Text);
+            szer = int.Parse(textBox4.Text);
+
+            int x = coordinates.X / (500 / wys);
+            int y = coordinates.Y / (500 / szer);
+
+            beginTab[x][y] = nextState;
+
+            g.Clear(Color.Transparent);
+            for (int z = 0; z < wys; z++)
+            {
+                for (int j = 0; j < szer; j++)
+                {
+                    if (beginTab[z][j] == nextState)
+                    {
+                        if (wys > szer)
+                            g.FillRectangle(pinkBrush, z * (500 / wys), j * (500 / wys), 500 / wys, 500 / wys);
+                        if (szer > wys)
+                            g.FillRectangle(pinkBrush, z * (500 / szer), j * (500 / szer), 500 / szer, 500 / szer);
+                    }
+                }
+            }
+            pictureBox1.Image = DrawArea;
+        }
     }
+
 }
+
